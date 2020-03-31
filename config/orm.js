e81b08a5f9d4c2d12605ade84953c243b507a0b3
Your Name <you@example.com>
2020-03-30 19:36:28 -0600

    Work on ORM.js file

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,19 @@
 var connection = require("./connection.js");
 
 function objToSql(ob) {
+
     var arr = [];
 
     for (var key in ob) {
+
         var value = ob[key];
 
-        if (Object.hasOwnProperty.call)
+        if (Object.hasOwnProperty.call(ob, key))
+
+            if (typeOf value === "string" && value.indexOf("") >= 0){
+                
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value)
     }
 }
