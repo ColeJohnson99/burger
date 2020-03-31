b24432a9245c4bb0127cdfc234769bffbd391f04
Your Name <you@example.com>
2020-03-30 18:19:49 -0600

    Set up server.js file with dependencies, routing, and handlebars

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-// *** Dependencies
-// =============================================================
+
 var express = require("express");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
 app.use(express.static("public"));
 
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
+var exphbs = require("express-handlebars");
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true}).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+require("./controllers/burgers_controllers.js")(app);
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App is now listening at: " + PORT)
+})
