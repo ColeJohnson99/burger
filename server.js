a62074d0791eaef70b73560884b4f488f453fefd
Your Name <you@example.com>
2020-03-31 11:30:27 -0600

    Fixed handlebars routing issue.  Image is not showing up though linked correctly

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-
-
+/*
 var express = require("express");
 
 var app = express();
@@ -26,3 +25,40 @@ app.use(routes);
 app.listen(PORT, function() {
     console.log("App is now listening at: " + PORT)
 })
+*/
+var express = require("express");
+var methodOver = require("method-override");
+var bodyParser = require("body-parser");
+var path = require('path');
+
+var app = express();
+
+var PORT = process.env.PORT || 3307;
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({
+    extname: 'handlebars', 
+    defaultLayout: 'index', 
+    layoutsDir: path.join(__dirname, 'views'),
+    partialsDir  : [
+        //  path to your partials
+        path.join(__dirname, 'views/partials'),
+    ]
+}));
+
+var routes = require("./controllers/burgers_controllers");
+
+app.use("/", routes);
+
+// The listener. Starts the server. 
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
